88124e56ca0532daf031212756e1bfc9d64ecc4e
aarnav singh <aarnavsingh836@gmail.com>
2023-10-25 17:56:19 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,4 @@
 'use strict';
-
-
-
-/**
- * PRELOAD
- * 
- * loading will be end after document is loaded
- */
-
 const preloader = document.querySelector("[data-preaload]");
 
 window.addEventListener("load", function () {
@@ -15,23 +6,12 @@ window.addEventListener("load", function () {
   document.body.classList.add("loaded");
 });
 
-
-
-/**
- * add event listener on multiple elements
- */
-
 const addEventOnElements = function (elements, eventType, callback) {
   for (let i = 0, len = elements.length; i < len; i++) {
     elements[i].addEventListener(eventType, callback);
   }
 }
 
-
-
-/**
- * NAVBAR
- */
 
 const navbar = document.querySelector("[data-navbar]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
@@ -44,12 +24,6 @@ const toggleNavbar = function () {
 }
 
 addEventOnElements(navTogglers, "click", toggleNavbar);
-
-
-
-/**
- * HEADER & BACK TOP BTN
- */
 
 const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
@@ -78,11 +52,6 @@ window.addEventListener("scroll", function () {
   }
 });
 
-
-
-/**
- * HERO SLIDER
- */
 
 const heroSlider = document.querySelector("[data-hero-slider]");
 const heroSliderItems = document.querySelectorAll("[data-hero-slider-item]");
@@ -122,9 +91,6 @@ const slidePrev = function () {
 
 heroSliderPrevBtn.addEventListener("click", slidePrev);
 
-/**
- * auto slide
- */
 
 let autoSlideInterval;
 
@@ -143,10 +109,6 @@ addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseout", autoSlide
 window.addEventListener("load", autoSlide);
 
 
-
-/**
- * PARALLAX EFFECT
- */
 
 const parallaxItems = document.querySelectorAll("[data-parallax-item]");
 
